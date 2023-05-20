cecdafcd750006d071a2b7bb41c172ad49cc9db3
Satan Sharma <satansharma7549@gmail.com>
2023-05-21 03:03:20 +0530

    Update AllRoutes.jsx

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../Pages/Home";
+
 import Login from "../Pages/Login";
+import Home from "../Pages/Home";
 
 const AllRoutes = () => {
   return (
