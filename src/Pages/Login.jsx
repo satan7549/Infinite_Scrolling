c48b3d80f1281c6eab0656959a92b26e0d80adf4
Satan Sharma <satansharma7549@gmail.com>
2023-05-20 22:42:27 +0530

    Create Login.jsx

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Box } from "@chakra-ui/react";
+
+const Login = () => {
+  // const [userName, setUserName] = useState("");
+  // const [userPassword, setUserPassword] = useState("");
+
+  return (
+    <Box paddingTop={"100px"} display={"flex"} flexDir={"column"} gap={2}>
+      Login
+    </Box>
+  );
+};
+
+export default Login;
