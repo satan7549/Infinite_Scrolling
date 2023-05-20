a0295fecbeb11dae74c232d22f695da9dbbdae05
Satan Sharma <satansharma7549@gmail.com>
2023-05-21 03:03:24 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box } from "@chakra-ui/react";
 
 const Login = () => {
-  // const [userName, setUserName] = useState("");
-  // const [userPassword, setUserPassword] = useState("");
-
   return (
     <Box paddingTop={"100px"} display={"flex"} flexDir={"column"} gap={2}>
       Login
