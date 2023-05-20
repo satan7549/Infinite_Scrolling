a856f3aa24cfa3b3d97ebe8a69f0d8654899a1fa
Satan Sharma <satansharma7549@gmail.com>
2023-05-20 22:42:24 +0530

    Create Home.jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Box paddingTop={"100px"} display={"flex"} flexDir={"column"} gap={2}>
+      Home
+    </Box>
+  );
+};
+
+export default Home;
