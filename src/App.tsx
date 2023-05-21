5e4d1aeebdbd4571e98755924454bd623e62677c
Satan Sharma <satansharma7549@gmail.com>
2023-05-21 17:53:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import Navbar from "./Components/Navbar";
 import { Box } from "@chakra-ui/react";
 import AllRoutes from "./Routes/AllRoutes";
 
+// App component is a entry point of the application
 const App: React.FC = () => {
   return (
-    <Box display="flex" flexDirection="column">
+    <Box display="flex" flexDirection="column" >
+      {/* The Navbar component is rendered at the top of the application */}
       <Navbar />
+      {/* The AllRoutes component is responsible for rendering different routes based on the current URL */}
       <AllRoutes />
     </Box>
   );
