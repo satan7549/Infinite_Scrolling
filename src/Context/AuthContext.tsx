c1c1fa054cc6b5b9a91c6864f02d5a8ff071537f
Satan Sharma <satansharma7549@gmail.com>
2023-05-21 17:53:44 +0530

    Update AuthContext.tsx

--- src/Context/AuthContext.tsx
+++ src/Context/AuthContext.tsx
@@ -1,27 +1,37 @@
 import React, { createContext, useEffect, useState, ReactNode } from "react";
 import { AuthContextType } from "../constains";
 
+// Create a context for authentication-related data and actions
 export const AuthContext = createContext<AuthContextType | null>(null);
 
+// Retrieve authentication state from local storage
 const authState = localStorage.getItem("authState");
 
+// AuthContextProvider component responsible for managing authentication state
 export const AuthContextProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(authState === "true");
+  // Set initial authentication state based on local storage value
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    authState === "true"
+  );
 
+  // Update local storage with the latest authentication state
   useEffect(() => {
     localStorage.setItem("authState", isAuthenticated.toString());
   }, [isAuthenticated]);
 
+  // Handle user login by updating the authentication state to true
   const login = () => {
     setIsAuthenticated(true);
   };
 
+  // Handle user logout by updating the authentication state to false
   const logout = () => {
     setIsAuthenticated(false);
   };
 
+  // Provide the authentication state and login/logout functions to child components
   return (
     <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
       {children}
