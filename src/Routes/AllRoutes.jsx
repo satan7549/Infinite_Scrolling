e7559934ad55d8a228aa4aa3af5217d415bb6d59
Satan Sharma <satansharma7549@gmail.com>
2023-05-21 13:31:19 +0530

    Update AllRoutes.jsx

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Login from "../Pages/Login";
 import Home from "../Pages/Home";
 import PrivateRoute from "./PrivateRoute";
 import ErrorPage from "../Pages/ErrorPage";
+import { AuthContext } from "../Context/AuthContext";
 
 const AllRoutes = () => {
+  const { isAuthenticated } = useContext(AuthContext);
   return (
     <Routes>
+      <Route path="/" element={isAuthenticated ? <Home /> : <Login />} />
       <Route
         path="/home"
         element={
