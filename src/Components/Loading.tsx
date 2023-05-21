565c7f3bd8ef5b31212f67ec0014b1c1202eedb1
Satan Sharma <satansharma7549@gmail.com>
2023-05-21 17:53:29 +0530

    Update Loading.tsx

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Box, Skeleton, Flex, Spacer } from "@chakra-ui/react";
 
+// Loading component responsible for rendering a set of skeleton loading cards
 const Loading: React.FC = () => {
   const skeletonCount = 10;
 
+  // Generate an array of skeleton loading cards based on the skeletonCount
   const skeletonCards = Array.from(Array(skeletonCount)).map((_, index) => (
     <Box key={index} borderWidth="1px" borderRadius="lg" p={4} mb={4}>
       <Flex alignItems="center">
