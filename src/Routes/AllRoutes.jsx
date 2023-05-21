a14e174d500885255ddd8f9abc8c4f09529dd2e6
Satan Sharma <satansharma7549@gmail.com>
2023-05-21 12:21:16 +0530

    Update AllRoutes.jsx

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -3,12 +3,22 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "../Pages/Login";
 import Home from "../Pages/Home";
+import PrivateRoute from "./PrivateRoute";
+import ErrorPage from "../Pages/ErrorPage";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route
+        path="/home"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
       <Route path="/login" element={<Login />} />
+      <Route path="/*" element={<ErrorPage />} />
     </Routes>
   );
 };
