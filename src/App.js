a83d9dc4a395538e919eaca1069ef0c8de2bf51c
Satan Sharma <satansharma7549@gmail.com>
2023-05-21 12:20:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import { Box } from "@chakra-ui/react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Login from "./Pages/Login";
-import Home from "./Pages/Home";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-  };
-
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-  };
-
   return (
     <Box display={"flex"} flexDirection={"column"}>
-      <Navbar isAuthenticated={isAuthenticated} onLogout={handleLogout} />
-      <Routes>
-        <Route
-          path="/login"
-          element={
-            <Login onLogin={handleLogin} isAuthenticated={isAuthenticated} />
-          }
-        />
-        <Route
-          path="/home"
-          element={
-            isAuthenticated ? <Home /> : <Navigate to="/login" replace={true} />
-          }
-        />
-      </Routes>
+      <Navbar />
+      <AllRoutes />
     </Box>
   );
 }
