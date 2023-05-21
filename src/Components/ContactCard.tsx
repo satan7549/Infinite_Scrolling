859617ee7daa18601abe8d1bc28fe429c233323f
Satan Sharma <satansharma7549@gmail.com>
2023-05-21 17:53:18 +0530

    Update ContactCard.tsx

--- src/Components/ContactCard.tsx
+++ src/Components/ContactCard.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { ListItem, Box, Flex, Spacer, Text, Avatar } from "@chakra-ui/react";
 import { Contact } from "../constains";
 
+// ContactCard component responsible for rendering an individual contact card
 interface ContactCardProps {
-  contact: Contact;
+  contact: Contact; // Contact object containing contact information
 }
 
 const ContactCard: React.FC<ContactCardProps> = ({ contact }) => {
-  const { name, picture } = contact;
+  const { name, picture } = contact; // Extracting name and picture from the contact object
 
   return (
     <ListItem>
@@ -18,8 +19,10 @@ const ContactCard: React.FC<ContactCardProps> = ({ contact }) => {
         boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
       >
         <Flex flexDirection="row" alignItems="center">
+          {/* Displaying the full name of the contact */}
           <Text>{`${name.first} ${name.last}`}</Text>
           <Spacer />
+          {/* Displaying the contact's profile picture */}
           <Avatar
             size="lg"
             name={`${name.first} ${name.last}`}
