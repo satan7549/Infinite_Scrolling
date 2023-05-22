a965776acbe01a0feb549118f33a89c86f712879
Satan Sharma <satansharma7549@gmail.com>
2023-05-22 14:21:55 +0530

    Update ContactList.tsx

--- src/Components/ContactList.tsx
+++ src/Components/ContactList.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { List, Box, Heading, UnorderedList } from "@chakra-ui/react";
+import { Grid, Box, Heading } from "@chakra-ui/react";
 import ContactCard from "./ContactCard";
-import Loading from "./Loading";
 import { Contact } from "../constains";
 
 // ContactList component responsible for rendering a list of contacts
@@ -12,25 +11,23 @@ interface ContactListProps {
 
 const ContactList: React.FC<ContactListProps> = ({ contacts, loading }) => {
   return (
-    <Box
-      mt={"80px"}
-      p={4}
-      width={{ base: "100%", sm: "320px", md: "500px", lg: "800px" }}
-      mx="auto"
-      boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"}
-    >
+    <Box h={"100vh"} mt={"80px"} p={4}>
       <Heading textAlign="center" mb={4}>
         Infinite Scroll Contacts
       </Heading>
-      <UnorderedList listStyleType="none" p={0}>
+      <Grid
+        templateColumns={{
+          base: "1fr",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(2, 1fr)",
+          lg: "repeat(3, 1fr)",
+        }}
+        gap={4}
+      >
         {contacts.map((contact, index) => (
-          <List key={index} spacing={3}>
-            <ContactCard contact={contact} />
-          </List>
+          <ContactCard key={index} contact={contact} loading={loading} />
         ))}
-      </UnorderedList>
-      {/* Render the Loading component if contacts are still loading*/}
-      {loading && <Loading />}
+      </Grid>
     </Box>
   );
 };
